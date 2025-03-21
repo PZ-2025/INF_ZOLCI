1f678a0cdc021185d65cd663f3244424555eeb07
Kacper Bułaś <kacper.bulas02@gmail.com>
2025-03-21 14:51:33 +0100

    build(database): added dependencies: JPA, Hibernate

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,6 +21,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // JPA + Hibernate + MariaDB
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.2.0")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+    runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
 }
 
 tasks.withType<Test> {
