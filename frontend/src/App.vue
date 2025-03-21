36116c7234a4ad68ff25d006673505196ca77fae
gubbl3bum <gb125101@stud.ur.edu.pl>
2025-03-21 13:01:02 +0100

    Update App.vue
    Change app.vue to support renamed navbar and other component next to it

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import Navbar from './components/Navbar.vue';
+import RaportHistory from './views/RaportHistory.vue';
+
+const currentView = ref('home');
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app-container">
+    <Navbar @navigate="currentView = $event" />
+    <div class="content">
+      <div v-if="currentView === 'home'">
+        <h1>Strona Główna</h1>
+        <p>Witaj w aplikacji!</p>
+      </div>
+      <ReportsView v-if="currentView === 'reports'" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
