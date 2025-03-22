a2ad1d05ba8681d86a3c295b142564357c445022
Rolaski <jakubowskijjal1@gmail.com>
2025-03-22 14:47:15 +0100

    create entity - Team

---
+++ backend/src/main/java/com/example/database/models/Team.java
@@ -0,0 +1,42 @@
+package com.example.database.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "teams")
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "manager_id", nullable = false)
+    private User manager;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "is_active")
+    private Boolean isActive = true;
+
+    @OneToMany(mappedBy = "team")
+    private Set<TeamMember> members = new HashSet<>();
+
+    @OneToMany(mappedBy = "team")
+    private Set<Task> tasks = new HashSet<>();
+
+}
