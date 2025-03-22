1fe3699abe1338a3605279dc2d77bb1d8fe8df38
Rolaski <jakubowskijjal1@gmail.com>
2025-03-22 17:45:44 +0100

    Fix autowiring issues by adding explicit ComponentScan for repository packages

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -2,8 +2,11 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.database", "com.example.database.repository"})
+
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
