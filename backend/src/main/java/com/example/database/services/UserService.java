e6c73995b5b204b574a7c189893a037188e173ba
Rolaski <jakubowskijjal1@gmail.com>
2025-03-22 18:25:46 +0100

    add service for user

---
+++ backend/src/main/java/com/example/database/services/UserService.java
@@ -0,0 +1,36 @@
+package com.example.database.services;
+
+import com.example.database.models.User;
+import com.example.database.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Integer id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Integer id) {
+        userRepository.deleteById(id);
+    }
+}
