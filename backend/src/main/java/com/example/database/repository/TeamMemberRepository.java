02154c3525bb811b493ed2a63039f4a5e32cef52
Rolaski <jakubowskijjal1@gmail.com>
2025-03-22 21:03:59 +0100

    adding findByUserAndTeam, findAllByTeam method for interface

--- backend/src/main/java/com/example/database/repository/TeamMemberRepository.java
+++ backend/src/main/java/com/example/database/repository/TeamMemberRepository.java
@@ -7,10 +7,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TeamMemberRepository extends JpaRepository<TeamMember, Integer> {
     List<TeamMember> findByUser(User user);
     List<TeamMember> findByTeam(Team team);
     List<TeamMember> findByTeamAndIsActive(Team team, boolean isActive);
+    Optional<TeamMember> findByUserAndTeam(User user, Team team);
+
+    List<TeamMember> findAllByTeam(Team team);
 }
