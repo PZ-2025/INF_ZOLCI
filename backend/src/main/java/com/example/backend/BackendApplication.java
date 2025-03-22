fd00a40480db6572776146490a1a316acc3bf94e
Rolaski <jakubowskijjal1@gmail.com>
2025-03-22 21:00:38 +0100

    add loading of environment variables for the entire backend

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -2,17 +2,33 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.database.repository")
+@EntityScan(basePackages = "com.example.database.models")
 @ComponentScan(basePackages = {
         "com.example.database",
-        "com.example.database.repository",
+        "com.example.database.controllers",
         "com.example.database.services"
 })
 
 public class BackendApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        // Ładowanie zmiennych środowiskowych
+        Dotenv dotenv = Dotenv.configure().load();
+
+        // Ustaw je jako właściwości systemowe
+        dotenv.entries().forEach(e ->
+                System.setProperty(e.getKey(), e.getValue())
+        );
+
+
         SpringApplication.run(BackendApplication.class, args);
     }
 }
