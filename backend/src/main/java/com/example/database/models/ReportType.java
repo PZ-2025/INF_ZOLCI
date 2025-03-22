bffa5d76bcd724f273eb966dece4ecf199b7fcb8
Rolaski <jakubowskijjal1@gmail.com>
2025-03-22 14:46:40 +0100

    create entity - ReportType

---
+++ backend/src/main/java/com/example/database/models/ReportType.java
@@ -0,0 +1,30 @@
+package com.example.database.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "report_types")
+public class ReportType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, unique = true, length = 100)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "template_path")
+    private String templatePath;
+
+    @OneToMany(mappedBy = "type")
+    private Set<Report> reports = new HashSet<>();
+}
