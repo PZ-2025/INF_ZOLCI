2cea7313aa1030a2e189b431f6144abe6e2450ed
Rolaski <jakubowskijjal1@gmail.com>
2025-03-22 21:01:05 +0100

    add dotenv to dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -23,10 +23,13 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.mariadb.jdbc:mariadb-java-client")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.3.2")
     implementation("org.liquibase:liquibase-core")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
+    implementation ("io.github.cdimascio:dotenv-java:2.3.2")
+
 }
 
 tasks.withType<Test> {
