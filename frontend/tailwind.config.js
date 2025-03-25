30cb7c7db7a69ad5fdef78ff4a166d438e41af96
gubbl3bum <gb125101@stud.ur.edu.pl>
2025-03-25 12:51:43 +0100

    Add colors to tailwind
    Add custom colors to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,10 +3,18 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-    ],  
+  ],  
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navbar: "#780116",
+        accent: "#F7B538",
+        secondary: "#DB7C26",
+        warning: "#D8572A",
+        danger: "#C32F27",
+        primary: "#cc7204",
+      },
+    },
   },
   plugins: [],
 }
-
