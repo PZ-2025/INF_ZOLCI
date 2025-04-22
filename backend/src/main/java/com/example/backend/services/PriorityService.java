493328caa9746d0022219a9f1b5699748965ea50
Mateusz Bocak <200119050+Mateusz-Bocak@users.noreply.github.com>
2025-04-22 23:41:36 +0200

    Refactor: enable package to use PrioritySerive map funcitons

--- backend/src/main/java/com/example/backend/services/PriorityService.java
+++ backend/src/main/java/com/example/backend/services/PriorityService.java
@@ -45,7 +45,7 @@ public class PriorityService {
      * @param priority Encja do mapowania
      * @return Obiekt DTO reprezentujący priorytet
      */
-    private PriorityDTO mapToDTO(Priority priority) {
+    protected PriorityDTO mapToDTO(Priority priority) {
         if (priority == null) return null;
 
         PriorityDTO dto = new PriorityDTO();
@@ -63,7 +63,7 @@ public class PriorityService {
      * @param dto Obiekt DTO do mapowania
      * @return Encja reprezentująca priorytet
      */
-    private Priority mapToEntity(PriorityDTO dto) {
+    protected Priority mapToEntity(PriorityDTO dto) {
         if (dto == null) return null;
 
         Priority priority = new Priority();
