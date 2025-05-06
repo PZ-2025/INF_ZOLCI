0ae07f6f591c4edb8d98a0d727a2181cce9b609a
gubbl3bum <gb125101@stud.ur.edu.pl>
2025-05-06 21:10:29 +0200

    Update Tailwind CSS configuration: add warning colors and import Vue warning

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import { warn } from 'vue';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -10,12 +12,15 @@ export default {
         background: "#F9FAFB",     // jasne tło aplikacji
         surface: "#FFFFFF",        // karty, panele
         primary: "#3F51B5",        // główny kolor akcji (np. przyciski)
-        accent: "#FFC107",         // podświetlenia, ikony, liczby
+        accent: "#7986CB",         // podświetlenia, ikony, liczby
         secondary: "#5C6BC0",      // nagłówki, menu, alternatywne przyciski
         success: "#4CAF50",        // status OK
         danger: "#F44336",         // błędy, ostrzeżenia
         text: "#1F2937",           // główny kolor tekstu
         muted: "#6B7280",          // pomocniczy tekst
+        warning: "#3949AB",        // ostrzeżenia
+        warningHover: "#303F9F",   // kolor ostrzeżenia po najechaniu
+        navbar: "#212A5C",         // kolor tła paska nawigacyjnego
       },
     },
   },
